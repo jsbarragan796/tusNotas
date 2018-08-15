849a7d793e0680717eac4d7fdf8be5bf31a025da
Sebastian Barragan Jeronimo <js.barragan796@uniandes.edu.co>
2018-08-15 14:03:51 -0500

    modelos y formularios

---
+++ notas/forms.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+from django.forms import ModelForm
+from django.contrib.auth.forms import forms
+from django.contrib.auth.models import User
+from notas.models import Evento
+from django.contrib.auth import password_validation
+
+
+class UserForm(ModelForm):
+
+    email = forms.EmailField(label="Correo electrónico",widget=forms.EmailField())
+    password = forms.CharField(label="Contraseña", widget=forms.PasswordInput())
+    password2 = forms.CharField(label="Confirmación de contraseña", widget=forms.PasswordInput())
+
+    class Meta:
+        model = User
+        fields = ['email', 'password', 'password2']
+
+    # verificacion correo unico
+    def verificar_email(self):
+        email = self.cleaned_data['email']
+        if User.objects.filter(email=email):
+            raise forms.ValidationError('Correo ya ha sido registrado')
+        return email
+
+    # verificacion las contraseñas coinciden y seguridad
+    def verificacion_password2(self):
+        password = self.cleaned_data['password']
+        password2 = self.cleaned_data['password2']
+        if password != password2:
+            raise forms.ValidationError('Contraseñas no coinciden')
+        try:
+            password_validation.validate_password(password2)
+        except password_validation.ValidationError as errores:
+            mensajes = []
+            for m in errores.messages:
+                if m == 'This password is too short. It must contain at least 8 characters.':
+                    mensajes.append('Contraseña muy corta, debe contener más de 7 caracteres')
+                if m == 'This password is too common.':
+                    mensajes.append('Contraseña muy común')
+                if m == 'This password is entirely numeric.':
+                    mensajes.append('Contraseña no puede contener solo números')
+                if m.startswith("The password is too similar"):
+                    mensajes.append('Contraseña muy similar a los datos del usuario')
+            raise forms.ValidationError(mensajes)
+        return password2
+
+
+class EventoForm(ModelForm):
+    email = forms.EmailField(label="Correo electrónico", widget=forms.EmailField())
+
+    nombre = forms.CharField(max_length=100)
+    lugar = forms.CharField(max_length=500)
+    direccion = forms.CharField(max_length=500)
+
+    fecha_inicio = forms.DateField()
+    fecha_terminacion = forms.DateField()
+    CATEGORIA_CHOICES = (
+        (1, 'Conferencia'),
+        (2, 'Seminario'),
+        (3, 'Congreso'),
+        (4, 'Curso')
+    )
+    categoria = forms.ChoiceField(label="Sistema Operativo",
+                              choices=CATEGORIA_CHOICES)
+
+    TIPO_CHOICES = (
+        (1, 'Virtual'),
+        (2, 'Presencial')
+    )
+    tipo = forms.ChoiceField(label="Tipo Evento", choices=TIPO_CHOICES)
--- notas/models.py
+++ notas/models.py
@@ -1,7 +1,37 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from django.db import models
 from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+
+class Evento(models.Model):
+    usuario = models.ForeignKey(User, null=False)
+    nombre = models.CharField(max_length=100, null=False, blank=False)
+    lugar = models.CharField(max_length=500, null=False, blank=False)
+    direccion = models.CharField(max_length=500, null=False, blank=False)
+
+    fecha_inicio = models.DateTimeField(null=True, blank=True)
+    fecha_terminacion = models.DateTimeField(null=True, blank=True)
+
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+    fecha_editado = models.DateTimeField(auto_now=True)
+
+    CATEGORIA_CHOICES = (
+        (1, 'Conferencia'),
+        (2, 'Seminario'),
+        (3, 'Congreso'),
+        (4, 'Curso')
+    )
+    categoria = models.PositiveSmallIntegerField(choices=CATEGORIA_CHOICES, null=False, blank=False)
+
+    TIPO_CHOICES = (
+        (1, 'Virtual'),
+        (2, 'Presencial')
+    )
+    tipo = models.PositiveSmallIntegerField(choices=TIPO_CHOICES, null=False, blank=False)
+
+    def comparar_fecha_creacion(self, otro_evento):
+        return self.fecha_creacion - otro_evento.fecha_creacion
+
